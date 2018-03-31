b0db298c0c78ef76b58e18173f6141c58d2da1db
Megabyte <vasiliy.p.kostin@gmail.com>
2018-03-31 13:52:39 +0300

    base boiler plate stetted up

--- index.js
+++ index.js
@@ -11,28 +11,48 @@ const rootPath = path.resolve(__dirname)+'/';
 var PORT = 32023;
 
 app.use('/', express.static(path.join(__dirname, 'public')));
-app.get('/exit', (req, res) => {
-	res.end();
-	log('Thing closing...');
-	server.close();
-});
-var clientLibs = {
-	'jquery': 'node_modules/jquery/dist/jquery'
+
+//========= libs reflection to public ===================================================
+var clientJsLibs = {
+	'react-dom.development.js': 'node_modules/react-dom/umd/react-dom.development.js',
+	'react.development.js': 'node_modules/react/umd/react.development.js',
+	'jquery.min.js': 'node_modules/jquery/dist/jquery.min.js',
+	'pixi.min.js': 'node_modules/pixi.js/dist/pixi.min.js'
 };
+createSymlink(rootPath + 'node_modules/reset-css/reset.css', 'public/css/reset.css');
+
 var libsFolder = rootPath + 'public/js/lib/';
-if (!fs.existsSync(libsFolder)){
+if (!fs.existsSync(libsFolder)) {
     fs.mkdirSync(libsFolder);
 }
-Object.keys(clientLibs).some((k) => {
-	var src = rootPath + clientLibs[k];
+function createSymlink(src, dest) {
+	if(fs.existsSync(src)) {
+		fs.symlink(src, dest, ef);
+	}
+}
+Object.keys(clientJsLibs).some((k) => {
+	var src = rootPath + clientJsLibs[k];
 	var dest = libsFolder + k;
-	log(src);
-	log(dest);
-	fs.symlink(src + '.js', dest + '.js', ef);
-	fs.symlink(src + '.min.js', dest + '.min.js', ef);
+	createSymlink(src, dest);
 });
 
-var server = app.listen(PORT, () => log('Example app listening on port ' + PORT + '!'))
+//========= start server ================================================================
+var server = app.listen(PORT, () => log('Example app listening on port ' + PORT + '!'));
 
 const opn = require('opn');
 opn('', {app: ['chrome', /*'--new-window --app=' +*/ 'http://127.0.0.1:' + PORT]});
+
+//======== socket connection with client ================================================
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: PORT + 1 });
+wss.on('connection', function connection(ws) {
+	ws.on('message', function incoming(message) {
+		console.log('received: %s', message);
+	});
+	/*ws.on('close', function onWsClose(){
+		log('Thing closing...');
+		server.close();
+		process.exit(0);
+	});*/
+	ws.send('something');
+});
---
+++ public/js/utils/socket.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const ws = new WebSocket('ws://' + location.hostname + ':' + (parseInt(location.port) + 1));
+
+ws.onopen = function open() {
+  ws.send('something');
+};
+
+ws.onmessage = function incoming(data) {
+  console.log(data);
+};
+
+ws.onclose = function incoming(data) {
+  window.close();
+};
+
+export default ws;
