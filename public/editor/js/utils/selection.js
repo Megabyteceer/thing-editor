42c8fe1b3075aa6f684f6a0746d45addfc27dd90
Megabyte <vasiliy.p.kostin@gmail.com>
2018-05-01 11:44:00 +0300

    tabs instead spaces

--- public/editor/js/utils/selection.js
+++ public/editor/js/utils/selection.js
@@ -17,36 +17,36 @@ class Selection extends Array {
 		recalculateNodesDeepness();
 		this.sort(sortByDeepness);
 	}
- 
+	
 	loadSelection(data) {
-        if(!data || data.length === 0) {
-            EDITOR.selection.clearSelection();
-        } else {
-            data.some(selectNodeByPath);
-        }
-        EDITOR.refreshTreeViewAndPropertyEditor();
-    }
-
-    saveSelection() {
-        return this.map(getPathOfNode);
-    }
-
+		if (!data || data.length === 0) {
+			EDITOR.selection.clearSelection();
+		} else {
+			data.some(selectNodeByPath);
+		}
+		EDITOR.refreshTreeViewAndPropertyEditor();
+	}
+	
+	saveSelection() {
+		return this.map(getPathOfNode);
+	}
+	
 	clearSelection(refresh) {
 		while (this.length > 0) {
 			this.remove(this[this.length - 1]);
 		}
-		if(refresh) {
-            EDITOR.refreshTreeViewAndPropertyEditor();
-        }
+		if (refresh) {
+			EDITOR.refreshTreeViewAndPropertyEditor();
+		}
 	}
 	
 	add(o) {
 		assert(!__getNodeExtendData(o).isSelected);
 		assert(this.indexOf(o) < 0);
-        __getNodeExtendData(o).isSelected = true;
-        if(o instanceof PIXI.Sprite) {
-            o.filters = selectedFilters;
-        }
+		__getNodeExtendData(o).isSelected = true;
+		if (o instanceof PIXI.Sprite) {
+			o.filters = selectedFilters;
+		}
 		this.push(o);
 	}
 	
@@ -54,10 +54,10 @@ class Selection extends Array {
 		assert(__getNodeExtendData(o).isSelected);
 		var i = this.indexOf(o);
 		assert(i >= 0);
-        __getNodeExtendData(o).isSelected = false;
-        if(o instanceof PIXI.Sprite) {
-            o.filters = null;
-        }
+		__getNodeExtendData(o).isSelected = false;
+		if (o instanceof PIXI.Sprite) {
+			o.filters = null;
+		}
 		this.splice(i, 1);
 	}
 }
@@ -68,34 +68,33 @@ const selectionFilter = new PIXI.filters.OutlineFilter(2, 0xffff00);
 const selectedFilters = [selectionFilter];
 
 setInterval(() => {
-    if(EDITOR.selection.length > 0) {
-        selectionFilter.color ^= 0x063311;
-    }
+	if (EDITOR.selection.length > 0) {
+		selectionFilter.color ^= 0x063311;
+	}
 }, 1000 / 60 * 3);
 
 //save/load selection
 
 var getPathOfNode = (node) => {
-    var ret = [];
-    while(node !== game.stage){
-        ret.push(node.parent.getChildIndex(node));
-        node = node.parent;
-    }
-    return ret;
+	var ret = [];
+	while (node !== game.stage) {
+		ret.push(node.parent.getChildIndex(node));
+		node = node.parent;
+	}
+	return ret;
 }
 
 var selectNodeByPath = (path, nodeNum) => {
-    var ret = game.stage;
-    for (var i = path.length-1; i >= 0; i--) {
-        ret = ret.getChildAt(path[i]);
-    }
-    if(nodeNum === 0) {
-        EDITOR.ui.sceneTree.selectInTree(ret);
-    } else {
-        EDITOR.selection.add(ret);
-    }
+	var ret = game.stage;
+	for (var i = path.length - 1; i >= 0; i--) {
+		ret = ret.getChildAt(path[i]);
+	}
+	if (nodeNum === 0) {
+		EDITOR.ui.sceneTree.selectInTree(ret);
+	} else {
+		EDITOR.selection.add(ret);
+	}
 }
-
 
 
 //-------- sorting selection --------------------------------
@@ -107,7 +106,7 @@ var recalculateNodesDeepness = () => {
 }
 
 var recalculateNodesDeepnessRecursive = (n) => {
-    __getNodeExtendData(n).deepness = curDeepness++;
+	__getNodeExtendData(n).deepness = curDeepness++;
 	if (n.hasOwnProperty('children')) {
 		n.children.some(recalculateNodesDeepnessRecursive);
 	}
