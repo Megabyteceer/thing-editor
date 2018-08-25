6d7ec43203913d9e1752a42343b168beb12b4b84
Megabyte <vasiliy.p.kostin@gmail.com>
2018-08-25 13:09:26 +0300

    resizing fix

--- js/ui/viewport.js
+++ js/ui/viewport.js
@@ -192,7 +192,7 @@ export default class Viewport extends React.Component {
 		let panel;
 		
 		let toggleOrientationBtn;
-		if(game && game.projDesc && (game.projDesc.screenOrientation === 'auto' && !game.projDesc.dynamicStageSize)) {
+		if(game && game.projDesc && (game.projDesc.screenOrientation === 'auto')) {
 			toggleOrientationBtn = R.btn(R.icon('orientation-toggle'), this.onToggleOrientationClick, 'Switch screen orientation (Ctrl + O)', 'big-btn', 1079);
 		}
 		
