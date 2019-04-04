13d31af4d912ac9c301971f741ee543efb6d850e
Megabyteceer <vasiliy.p.kostin@gmail.com>
2019-04-04 10:15:22 +0300

    react switched to production mode

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -10,6 +10,8 @@ var clientLibs = [
 	['../../node_modules/react-dom/umd/react-dom.development.js', 'js/lib/react-dom.development.js'],
 	['../../node_modules/react/umd/react.development.js', 'js/lib/react.development.js'],
 	['../../node_modules/reset-css/reset.css', 'css/lib/reset.css'],
+	['../../node_modules/react-dom/umd/react-dom.production.min.js', 'js/lib/react-dom.production.min.js'],
+	['../../node_modules/react/umd/react.production.min.js', 'js/lib/react.production.min.js'],
 
 	['../../node_modules/webfontloader/webfontloader.js', '../thing-engine/js/lib/webfontloader.js'],
 	['../../node_modules/howler/dist/howler.core.min.js', '../thing-engine/js/lib/howler.core.min.js'],
