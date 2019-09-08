018d279d2a815c9acf9fda359ebe612d25651ec7
Megabyteceer <vasiliy.p.kostin@gmail.com>
2019-09-08 13:41:48 +0300

    ref field value inptovement

--- js/ui/props-editor/ref-field-editor.js
+++ js/ui/props-editor/ref-field-editor.js
@@ -18,20 +18,36 @@ export default class RefFieldEditor extends React.Component {
 					}
 				}
 			));
-		} else if(val && typeof val === 'object') {
-			return R.btn("[Object...]", () => {
+		} else {
+
+			let title;
+			if(val) {
+				if(typeof val === 'object') {
+					title = '[object]';
+				} else if(typeof val === 'function') {
+					title = 'function: ' + val.name;
+				} else {
+					title = '' + val;
+				}
+			} else {
+				title = '' + val;
+			}
+
+			return R.btn(title, () => {
 				if(this.props.value) {
-					try {
-						editor.ui.modal.showModal(JSON.stringify(this.props.value, undefined, '\n'). split('\n').map((l) => {
-							return R.span(null,l, R.br());
-						}));
-					} catch (er) {
-						editor.ui.modal.showModal('Object has curcular structures');
+					if(React.isValidElement(this.props.value)) {
+						editor.ui.modal.showModal(this.props.value);
+					} else {
+						try {
+							editor.ui.modal.showModal(JSON.stringify(this.props.value, undefined, '\n'). split('\n').map((l) => {
+								return R.span(null,l, R.br());
+							}));
+						} catch (er) {
+							editor.ui.modal.showModal('Object has curcular structures');
+						}
 					}
 				}
 			});
-		} else {
-			return R.input({disabled:true, value: JSON.stringify(val)});
 		}
 	}
 }
