3f3ea4043d02ccee6fc09b089ab59d8c1348908e
Megabyteceer <vasiliy.p.kostin@gmail.com>
2019-10-19 18:43:51 +0300

    browser time imports directly from node-modules

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,6 +3,9 @@
 /*global __filename */
 /*global process */
 
+const path = require('path');
+const fs = require('fs');
+
 if((process.argv.indexOf('-access-granted') < 0) && process.platform.startsWith('win')) {
 	console.log('Windows detected. Attempt to grant admin access ' + process.platform);
 	var sudo = require('sudo-prompt');
@@ -16,50 +19,36 @@ if((process.argv.indexOf('-access-granted') < 0) && process.platform.startsWith(
 		}
 	);
 } else {
-	console.log('Acсess for making symlinks granted');
+	console.log('Access for making symlinks granted');
 
 	const symlinksMaker = require('./symlinks-maker.js');
-	const path = require('path');
-	const fs = require('fs');
 
 	var clientLibs = [
 		['./thing-engine/node_modules', '../node_modules', 'dir'],
-		['../../thing-engine', '../node_modules/thing-engine', 'dir'],
-		['../../node_modules/pixi-filters/dist/pixi-filters.js', 'js/lib/pixi-filters.js'],
-		['../../node_modules/react-dom/umd/react-dom.development.js', 'js/lib/react-dom.development.js'],
-		['../../node_modules/react/umd/react.development.js', 'js/lib/react.development.js'],
-		['../../node_modules/reset-css/reset.css', 'css/lib/reset.css'],
-		['../../node_modules/react-dom/umd/react-dom.production.min.js', 'js/lib/react-dom.production.min.js'],
-		['../../node_modules/react/umd/react.production.min.js', 'js/lib/react.production.min.js'],
-
-		['../../node_modules/webfontloader/webfontloader.js', '../thing-engine/js/lib/webfontloader.js'],
-		['../../node_modules/howler/dist/howler.core.min.js', '../thing-engine/js/lib/howler.core.min.js'],
-		['../../node_modules/pixi.js/dist/pixi.min.js', '../thing-engine/js/lib/pixi.min.js'],
-		['../../node_modules/pixi.js/dist/pixi.js', '../thing-engine/js/lib/pixi.js'],
-		['../../node_modules/pixi-spine/bin/pixi-spine.js', '../thing-engine/js/lib/pixi-spine.js']
+		['../../thing-engine', '../node_modules/thing-engine', 'dir']
 	];
 
 	symlinksMaker.setRootPath(path.resolve(__dirname) + '/../');
 	symlinksMaker.makeSymlinks(clientLibs);
 
-	function copyRecursiveSync(src, dest) {
-		var exists = fs.existsSync(src);
-		var stats = exists && fs.statSync(src);
-		var isDirectory = exists && stats.isDirectory();
-		if (exists && isDirectory) {
-			if (!fs.existsSync(dest)) {
-				fs.mkdirSync(dest);
-			}
-			fs.readdirSync(src).forEach(function (childItemName) {
-				copyRecursiveSync(path.join(src, childItemName),
-					path.join(dest, childItemName));
-			});
-		} else {
-			if (!fs.existsSync(dest)) {
-				fs.linkSync(src, dest);
-			}
+	copyRecursiveSync(path.join(__dirname, 'vscode.folder.settings'), path.join(__dirname, '../../'));
+}
+
+function copyRecursiveSync(src, dest) {
+	var exists = fs.existsSync(src);
+	var stats = exists && fs.statSync(src);
+	var isDirectory = exists && stats.isDirectory();
+	if (exists && isDirectory) {
+		if (!fs.existsSync(dest)) {
+			fs.mkdirSync(dest);
+		}
+		fs.readdirSync(src).forEach(function (childItemName) {
+			copyRecursiveSync(path.join(src, childItemName),
+				path.join(dest, childItemName));
+		});
+	} else {
+		if (!fs.existsSync(dest)) {
+			fs.linkSync(src, dest);
 		}
 	}
-
-	copyRecursiveSync(path.join(__dirname, 'vscode.folder.settings'), path.join(__dirname, '../../'));
 }
