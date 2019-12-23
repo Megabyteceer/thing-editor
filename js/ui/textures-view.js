61938cc5193a8cb369afe267da5f85df775deec6
Megabyteceer <vasiliy.p.kostin@gmail.com>
2019-12-23 17:22:38 +0300

    do not warn not included to build textures as unused

--- js/ui/textures-view.js
+++ js/ui/textures-view.js
@@ -284,6 +284,9 @@ class TexturesViewerBody extends React.Component {
 			checkListForImages(Lib.scenes);
 	
 			for(let imageName of allTextures) {
+				if(Lib.getTexture(imageName).__noIncludeInToBuild) {
+					continue;
+				}
 				editor.ui.status.warn('No refs to: ' + imageName, 32043, () => {
 					let path = this.getImagePath(imageName);
 					let view = R.img({src: path, className:'textures-viewer-image'});
