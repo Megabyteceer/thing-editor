db9ebbc9008ac3abc52174b7b6339d8791c154ad
Megabyteceer <vasiliy.p.kostin@gmail.com>
2020-02-11 11:01:28 +0300

    do not list dot started files

--- index.js
+++ index.js
@@ -348,12 +348,14 @@ function enumFiles() {
 
 const walkSync = (dir, fileList = []) => {
 	fs.readdirSync(dir).forEach(file => {
-		let stats = fs.statSync(path.join(dir, file));
-		
-		if(stats.isDirectory()) {
-			fileList = walkSync(path.join(dir, file), fileList);
-		} else if(stats.size > 0) {
-			fileList.push({name: path.join(dir, file), mtime: stats.mtimeMs});
+		if(!file.startsWith('.')) {
+			let fullPath = path.join(dir, file);
+			let stats = fs.statSync(fullPath);
+			if(stats.isDirectory()) {
+				fileList = walkSync(fullPath, fileList);
+			} else if(stats.size > 0) {
+				fileList.push({name: fullPath, mtime: stats.mtimeMs});
+			}
 		}
 	});
 	return fileList;
