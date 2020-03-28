a7dbc10d273b4a26efb9a540feb4b7b631995395
Megabyteceer <vasiliy.p.kostin@gmail.com>
2020-03-28 17:54:48 +0300

    Revert "load custom classes via dynamic import"
    This reverts commit cc1161bca02e50d48f375e75f6e4be04f93c5a4b.

--- js/editor/utils/classes-loader.js
+++ js/editor/utils/classes-loader.js
@@ -42,6 +42,8 @@ import MultilineText from 'thing-editor/js/engine/components/multiline-text.js';
 import StaticTrigger from 'thing-editor/js/engine/components/static-trigger.js';
 import BitmapText from 'thing-editor/js/engine/components/bitmap-text.js';
 
+let attachedScript;
+
 let ClassesLoader = {};
 ClassesLoader.initClassesLoader = function initClassesLoader() {
 	//embedded engine classes
@@ -150,6 +152,10 @@ function addClass(c, path) {
 		}
 	}
 	
+	if (path && (path.indexOf('/thing-editor/') !== 0)) {
+		console.log('Custom class loaded: ' + name + '; ' + path);
+	}
+	
 	classPathById[name] = (((typeof path) === 'string') ? path : false);
 	classesById[name] = c;
 	
@@ -252,77 +258,93 @@ function reloadClasses() { //enums all js files in src folder, detect which of t
 	return new Promise((resolve) => {
 		cacheCounter++;
 		currentLoadingPromiseResolver = resolve;
-
-		errorOccurred = false;
-	
-		loadedClassesCount = 0;
-		clearClasses();
-		enumClassProperties(DisplayObject);
-		enumClassProperties(Delay);
-		enumClassProperties(SceneLinkedPromise);
-		embeddedClasses.some((a) => {
-			let c = a[0];
-			if(!c.___EDITOR_isGoodForCallbackChooser && !c.___EDITOR_isGoodForChooser) {
-				c.___EDITOR_isHiddenForChooser = true;
-			}
-			embeddedClassesMap.set(c, true);
-			addClass(c, a[1]);
-		});
-	
-		window.onerror = function loadingErrorHandler(message, source, lineno, colno) {
-
-			let fn = loadedPath || source.split('?').shift().split(':' + location.port).pop();
-			editor.fs.editFile(fn, lineno, colno);
-			showError(R.fragment(
-				'Attempt to load: ' + loadedPath + ': ' + message,
-				R.div({className: 'error-body'}, fn + ' (' + lineno + ':' + colno + ')', R.br(), message),
-				'Please fix error in source code and press button to try again:'
-			), 30009);
-			loadedPath = null;
-		};
-	
-		let scriptSource = [];
-		let classesList = editor.fs.filesExt['src/game-objects'].concat(editor.fs.filesExt['src/scenes']);
-		classesList.forEach((classInfo, i) => {
-			let classPath = classInfo.name;
-			if(classPath.endsWith('.js')) {
-				let wrongSymbolPos = classPath.search(/[^a-zA-Z_\-\.\d\/]/gm);
-				if(wrongSymbolPos >= 0) {
-					editor.ui.status.warn("File " + classPath + " ignored because of wrong symbol '" + classPath[wrongSymbolPos] + "' in it's name", 32044, () => {
-						editor.fs.editFile(classPath);
-					});
-				} else {
-					scriptSource.push("import C" + i + " from '" + location.origin + '/' + classPath + "?v=" + (cacheCounter) + "'; editor.ClassesLoader.classLoaded(C" + i + ", '" + classPath + "', '" + (classInfo.lib || '') + "');");
+		setTimeout(() => {
+			errorOccurred = false;
+		
+			loadedClassesCount = 0;
+			clearClasses();
+			console.clear();
+			console.log('%c editor: classes loading begin:', 'font-weight:bold; padding:10px; padding-right: 300px; font-size:130%; color:#040; background:#cdc;');
+		
+			enumClassProperties(DisplayObject);
+			enumClassProperties(Delay);
+			enumClassProperties(SceneLinkedPromise);
+			embeddedClasses.some((a) => {
+				let c = a[0];
+				if(!c.___EDITOR_isGoodForCallbackChooser && !c.___EDITOR_isGoodForChooser) {
+					c.___EDITOR_isHiddenForChooser = true;
 				}
-			}
-		});
-	
-		let src = 'data:application/javascript,' + encodeURIComponent(scriptSource.join(';'));
-	
-		editor.ui.modal.showSpinner();
-		let loadedFine;
-		let d = import(src).then(() => { // eslint-disable-line
-			if(!errorOccurred) {
-				loadedFine = true;
-				Lib._setClasses(classesById, classesDefaultsById);
-			
-				classesLoadedSuccessfullyAtLeastOnce = true;
-				console.log(loadedClassesCount + ' custom components total loaded.');
-				resolve();
-			
-				editor.ui.classesList.forceUpdate();
-				game.__destroyCurrentScene();
-				Pool.clearAll();
-			} else {
-				console.warn('classes were not loaded because of error.');
-			}
-		}).finally(() => {
-			editor.ui.modal.hideSpinner();
-			window.onerror = null;	
-			if(!loadedFine) {
+				embeddedClassesMap.set(c, true);
+				addClass(c, a[1]);
+			});
+		
+			window.onerror = function loadingErrorHandler(message, source, lineno, colno) {
+
+				let fn = loadedPath || source.split('?').shift().split(':' + location.port).pop();
+				editor.fs.editFile(fn, lineno, colno);
+				showError(R.fragment(
+					'Attempt to load: ' + loadedPath + ': ' + message,
+					R.div({className: 'error-body'}, fn + ' (' + lineno + ':' + colno + ')', R.br(), message),
+					'Please fix error in source code and press button to try again:'
+				), 30009);
+				loadedPath = null;
+			};
+		
+			let scriptSource = '';
+			let classesList = editor.fs.filesExt['src/game-objects'].concat(editor.fs.filesExt['src/scenes']);
+			classesList.forEach((classInfo, i) => {
+				let classPath = classInfo.name;
+				if(classPath.endsWith('.js')) {
+					let wrongSymbolPos = classPath.search(/[^a-zA-Z_\-\.\d\/]/gm);
+					if(wrongSymbolPos >= 0) {
+						editor.ui.status.warn("File " + classPath + " ignored because of wrong symbol '" + classPath[wrongSymbolPos] + "' in it's name", 32044, () => {
+							editor.fs.editFile(classPath);
+						});
+					} else {
+						scriptSource += ("import C" + i + " from '" + location.origin + '/' + classPath + "?v=" + (cacheCounter) + "'; editor.ClassesLoader.classLoaded(C" + i + ", '" + classPath + "', '" + (classInfo.lib || '') + "');");
+					}
+				}
+			});
+		
+			let src = 'data:application/javascript,' + encodeURIComponent(scriptSource);
+		
+			let script = document.createElement('script');
+			editor.ui.modal.showSpinner();
+			script.onerror = function() {
 				showError("Can not load script; Please check Browser's console for error details, and fix problem to continue.", 31001);
+				script.onload();
+			};
+			script.onload = function() {
+			
+				editor.ui.modal.hideSpinner();
+				head.removeChild(script);
+			
+				window.onerror = null;
+				if(!errorOccurred) {
+					Lib._setClasses(classesById, classesDefaultsById);
+				
+					classesLoadedSuccessfullyAtLeastOnce = true;
+				
+					console.log('Loading success.');
+					console.log(loadedClassesCount + ' classes total.');
+					resolve();
+				
+					editor.ui.classesList.forceUpdate();
+					game.__destroyCurrentScene();
+					Pool.clearAll();
+				} else {
+					console.warn('classes were not loaded because of error.');
+				}
+			};
+			if(attachedScript) {
+				attachedScript.remove();
 			}
-		});
+			attachedScript = script;
+			script.type = 'module';
+			script.src = src;
+			head.appendChild(script);
+
+		},10);
 	});
 }
 let loadedPath;
