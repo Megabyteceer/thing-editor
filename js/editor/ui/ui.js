b59c3a973ad3f23990bbdac966c3f77532eaef36
Megabyteceer <vasiliy.p.kostin@gmail.com>
2020-06-24 08:41:26 +0300

    editor startup crash fix

--- js/editor/ui/ui.js
+++ js/editor/ui/ui.js
@@ -128,7 +128,9 @@ class UI extends React.Component {
 			renderWindow('sceneTree', 'SceneTree', 'Scene tree', React.createElement(TreeView, {ref: this.sceneTreeRef}), 0, 35, 250, 500, 250, 500),
 			renderWindow('viewport', 'Viewport', R.span(null, 'Viewport: ', editor.projectDesc ? R.b(null, editor.currentSceneName) : undefined, React.createElement(StatusBar)), React.createElement(Viewport, {ref: this.viewportRef}),
 				558, 0, 470, 535, 1362, 742, ()=>{
-					game._onContainerResize();
+					if(game.projectDesc) {
+						game._onContainerResize();
+					}
 				}),
 			renderWindow('propsEditor', 'Properties', 'Properties', React.createElement(PropsEditor, {
 				ref: this.propsEditorRef,
