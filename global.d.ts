27722254bcbae62a93bec7aa866231cf2eed8e80
Megabyteceer <vasiliy.p.kostin@gmail.com>
2020-11-04 16:24:08 +0300

    timeline loop-arrows colors variation

--- global.d.ts
+++ global.d.ts
@@ -1,12 +1,7 @@
-interface Game {
-    isPortrait: boolean;
-}
-
 import { DisplayObject } from 'pixi.js-legacy';
 import Editor from 'thing-editor/js/editor/editor.js';
 import {Game} from 'thing-editor/js/engine/game.js';
 import Container from './js/engine/components/container';
-
 
 class TEditor extends Editor {
 	game:Game;
@@ -17,5 +12,6 @@ class TEditor extends Editor {
 declare global {
 	var editor: TEditor;
 	function assert(expression: boolean, message?: string, errorCode?: number);
-	function __getNodeExtendData(node:Container): any
+	function __getNodeExtendData(node:Container): any;
+
 }
