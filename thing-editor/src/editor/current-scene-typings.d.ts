ad9f1a2ccbb048c82826ac9b2d8c9de648082122
vasili.kostin <vasili.kostin@bgaming.com>
2023-06-23 21:19:51 +0300

    Lib.loadPrefab typings generation Scene.all typings generation

--- thing-editor/src/editor/current-scene-typings.d.ts
+++ thing-editor/src/editor/current-scene-typings.d.ts
@@ -1,11 +1,24 @@
 // thing-editor auto generated file.
 
 import { Container } from "pixi.js";
-
+import MovieClip from "thing-editor/src/engine/lib/movie-clip/movie-clip.c";
+import Sprite from "thing-editor/src/engine/lib/sprite.c";
+import Shape from "thing-editor/src/engine/lib/shape.c";
+import Text from "thing-editor/src/engine/lib/basic/text.c";
+			
 declare global {
-	type CurrentSceneType = Scene;
+type CurrentSceneType = Scene;
 
-	interface ThingSceneAllMap {
-		[key: string]: Container;
-	}
+interface ThingSceneAllMap {
+	[key: string]: Container;
+"mc":MovieClip;
+"bg":Sprite;
+"bar":Sprite;
+"cap":Sprite;
+"prefab2":Shape;
+/** @deprecated Refused because 2 objects with that name present on the scene. */
+"prefab3":Shape;
+"prefab4":Shape;
+"txt":Text;
+}
 }
--- thing-editor/src/editor/prefabs-typing.ts
+++ thing-editor/src/editor/prefabs-typing.ts
@@ -1,6 +1,19 @@
 // thing-editor auto generated file.
 import Container from "thing-editor/src/engine/lib/container.c";
+import ProgressBar from "thing-editor/src/engine/lib/extended/progress-bar.c";
+import __BackDrop from "thing-editor/src/engine/lib/___system/backdrop.c";
+import ___Guide from "thing-editor/src/engine/lib/___system/guide.c";
+import MovieClip from "thing-editor/src/engine/lib/movie-clip/movie-clip.c";
+import Shape from "thing-editor/src/engine/lib/shape.c";
 export default class TLib {
+	static loadPrefab(prefabName: 'fader/default'):Container;
+	static loadPrefab(prefabName: '___default_content/progressbar'):ProgressBar;
+	static loadPrefab(prefabName: '___system/backdrop'):__BackDrop;
+	static loadPrefab(prefabName: '___system/gizmo'):Container;
+	static loadPrefab(prefabName: '___system/guide'):___Guide;
+	static loadPrefab(prefabName: '___system/unknown-prefab'):Container;
+	static loadPrefab(prefabName: 'prefab1'):MovieClip;
+	static loadPrefab(prefabName: 'prefab2'):Shape;
 	static loadPrefab(prefabName: string): Container;
 	static loadPrefab(prefabName: string): Container {
 		return prefabName as any;
