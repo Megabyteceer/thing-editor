0fd2f7fa9c2c9026d397a382ef39ea3d0897366b
vasili.kostin <vasili.kostin@bgaming.com>
2023-06-28 10:53:28 +0300

    SceneLinkedRequest

---
+++ thing-editor/src/engine/utils/scene-linked-request.ts
@@ -0,0 +1,19 @@
+import { Container } from "pixi.js";
+import assert from "thing-editor/src/engine/debug/assert";
+import SceneLinkedPromise from "./scene-linked-promise";
+
+
+
+export default class SceneLinkedRequest {
+
+	static fetch(owner: Container, url: string, resultFormat: ('json' | 'text' | 'blob' | 'arrayBuffer') = "json", options?: RequestInit) {
+		assert(owner, "Request's owner display object should be provided.", 10062);
+		assert(url, "Request's URL should be provided.", 10063);
+		let ret = SceneLinkedPromise.promise((resolve, reject) => {
+			fetch(url, options)
+				.then((response) => (response as any)[resultFormat]()).then(resolve).catch(reject);
+		}, owner);
+		ret.name = "Request: " + url;
+		return ret;
+	}
+}
