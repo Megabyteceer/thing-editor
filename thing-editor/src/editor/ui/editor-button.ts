64eee8a150f72b58dc14a1a489fe92dcc76ba123
vasili.kostin <vasili.kostin@bgaming.com>
2023-07-04 22:16:59 +0300

    timeline hotkeys fix

--- thing-editor/src/editor/ui/editor-button.ts
+++ thing-editor/src/editor/ui/editor-button.ts
@@ -44,6 +44,13 @@ const findMenuItemForHotkey = (hotkey: Hotkey): ContextMenuItem | undefined => {
 
 window.addEventListener("keydown", (ev) => {
 	if(ev.key !== 'Control' && ev.key !== 'Alt' && ev.key !== 'Shift') {
+
+		for(let b of allHotkeyButtons) {
+			if(b.onKeyDown(ev)) { //call only first button with this hotkey
+				return;
+			}
+		}
+
 		const item = findMenuItemForHotkey(ev as Hotkey);
 		if(item) {
 			item.onClick();
@@ -51,11 +58,7 @@ window.addEventListener("keydown", (ev) => {
 			return;
 		}
 
-		for(let b of allHotkeyButtons) {
-			if(b.onKeyDown(ev)) { //call only first button with this hotkey
-				return;
-			}
-		}
+
 	}
 });
 
