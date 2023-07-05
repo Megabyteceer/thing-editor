6db26f97d9dd87f4d3080c716bda04b3d4b3e86f
vasili.kostin <vasili.kostin@bgaming.com>
2023-07-05 14:05:24 +0300

    sure question embed prefab

--- thing-editor/src/editor/prefabs-typing.ts
+++ thing-editor/src/editor/prefabs-typing.ts
@@ -1,8 +1,10 @@
 // thing-editor auto generated file.
 import Container from "thing-editor/src/engine/lib/assets/src/basic/container.c";
+import Resizer from "thing-editor/src/engine/lib/assets/src/extended/resizer.c";
 import Flow from "games/slot-client/assets/src/custom/flow.c";
 export default class TLib {
 	static loadPrefab(prefabName: 'fader/default'):Container;
+	static loadPrefab(prefabName: 'ui/sure-question'):Resizer;
 	static loadPrefab(prefabName: 'flow-animation1'):Flow;
 	static loadPrefab(prefabName: string): Container;
 	static loadPrefab(prefabName: string): Container {
