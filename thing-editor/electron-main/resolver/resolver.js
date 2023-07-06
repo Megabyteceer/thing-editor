6c42be2fc6ee08527471f4e46783d1daa1e9e9cc
vasili.kostin <vasili.kostin@bgaming.com>
2023-07-06 18:47:49 +0300

    vite imports dedupe fix

--- thing-editor/electron-main/resolver/resolver.js
+++ thing-editor/electron-main/resolver/resolver.js
@@ -2,8 +2,13 @@ const moduleImportFixer = /(^\s*import.+from\s*['"][^'"]+)(['"])/gm;
 module.exports = {
 	name: 'thing-editor:resolver',
 	transform(src, id) {
-		if(id.indexOf('.c.ts?v=') > 0) {
-			const query = '?import&v=' + id.split('.ts?v=')[1];
+		if(id.indexOf('.ts?') > 0) {
+			let query = id.split('.ts?')[1];
+			if(query.indexOf('import&') < 0) {
+				query = '?import&' + query;
+			} else {
+				query = '?' + query;
+			}
 			src = src.replace(moduleImportFixer, (_substr, m1, m2) => {
 				if(m1.indexOf('from "games/') < 0 && m1.indexOf('from "libs/') < 0) {
 					return m1 + m2;
