f71481d0e87cef732eb60c10a18f26971ae45cd4
Vasiliy Kostin <vasiliy.p.kostin@gmail.com>
2023-09-06 13:57:31 +0300

    std lib remove l18n dependency BackDrop serialization fix

--- thing-editor/src/engine/lib/assets/src/basic/back-drop.c.ts
+++ thing-editor/src/engine/lib/assets/src/basic/back-drop.c.ts
@@ -1,4 +1,5 @@
 import { Point, Renderer } from "pixi.js";
+import type { SerializedObject } from "thing-editor/src/editor/env";
 import game from "thing-editor/src/engine/game";
 import Shape from "thing-editor/src/engine/lib/assets/src/extended/shape.c";
 
@@ -6,6 +7,16 @@ const zeroPoint = new Point();
 const sizePoint = new Point();
 
 export default class BackDrop extends Shape {
+
+	/// #if EDITOR
+	__afterSerialization(data: SerializedObject): void {
+		delete data.p.width;
+		delete data.p.height;
+		delete data.p.x;
+		delete data.p.y;
+	}
+
+	/// #endif
 
 	render(renderer: Renderer): void {
 		this.parent.toLocal(zeroPoint, game.stage, this, false);
