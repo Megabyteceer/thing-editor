2cd4b981056c119161f16fa475da7444ab05f7d6
Megabyteceer <vasiliy.p.kostin@gmail.com>
2023-09-29 16:20:28 +0300

    LayeredContainer

---
+++ thing-editor/src/engine/lib/assets/src/extended/layered-container.c.ts
@@ -0,0 +1,83 @@
+
+import { Container } from "pixi.js";
+import { SourceMappedConstructor } from "thing-editor/src/editor/env";
+import editable from "thing-editor/src/editor/props-editor/editable";
+import assert from "thing-editor/src/engine/debug/assert";
+import Lib from "thing-editor/src/engine/lib";
+import LayeredContainerPortal from "thing-editor/src/engine/lib/assets/src/extended/layered-contaiter-portal.c";
+import getValueByPath from "thing-editor/src/engine/utils/get-value-by-path";
+
+export default class LayeredContainer extends Container {
+
+	@editable({ type: 'data-path' })
+	set targetContainer(v: string) {
+		this._targetContainer = v;
+		this._updateTargetContainer();
+	}
+
+	get targetContainer() {
+		return this._targetContainer;
+	}
+
+	_targetContainer: string = null!;
+
+	@editable()
+	enabled = true;
+
+	rendererPortalContainer!: LayeredContainerPortal;
+
+	init() {
+		super.init();
+		this.rendererPortalContainer = Lib._loadClassInstanceById('LayeredContainerPortal') as LayeredContainerPortal;
+		this.rendererPortalContainer.containerOwner = this;
+		this._updateTargetContainer();
+	}
+
+	enable() {
+		this.enabled = true;
+		this._updateTargetContainer();
+	}
+
+	disable() {
+		this.enabled = false;
+		this._updateTargetContainer();
+	}
+
+	isRenderingLayered() {
+		return this.rendererPortalContainer && this._targetContainer && this.enabled;
+	}
+
+	_updateTargetContainer() {
+		if(this.rendererPortalContainer) {
+			if(this._targetContainer && this.enabled) {
+				let c = getValueByPath(this._targetContainer, this);
+				assert(c, 'Invalid targetContainer data-path value: ' + this._targetContainer);
+				if(this.rendererPortalContainer.parent !== c) {
+					c.addChild(this.rendererPortalContainer);
+				}
+			}
+		}
+	}
+
+	render(renderer: any) {
+		if(!this.isRenderingLayered()) {
+			super.render(renderer);
+		}
+	}
+
+	renderForPortal(renderer: any) {
+		this.visible = this.parent.worldVisible;
+		super.render(renderer);
+	}
+
+	onRemove() {
+		if(this.rendererPortalContainer) {
+			this.rendererPortalContainer.removeWithoutHolder();
+		}
+		super.onRemove();
+	}
+}
+/// #if EDITOR
+(LayeredContainer as any as SourceMappedConstructor).__requiredComponents = [LayeredContainerPortal];
+
+/// #endif
---
+++ thing-editor/src/engine/lib/assets/src/extended/layered-contaiter-portal.c.ts
@@ -0,0 +1,28 @@
+
+import { Container } from "pixi.js";
+import editable from "thing-editor/src/editor/props-editor/editable";
+import LayeredContainer from "thing-editor/src/engine/lib/assets/src/extended/layered-container.c";
+
+export default class LayeredContainerPortal extends Container {
+
+	@editable({ type: 'ref' })
+	containerOwner!: LayeredContainer;
+
+	render(renderer: any) {
+		if(this.containerOwner.isRenderingLayered()) {
+			this.containerOwner.renderForPortal(renderer);
+		}
+	}
+
+	onRemove() {
+		super.onRemove();
+		this.containerOwner.rendererPortalContainer = null as any;
+		this.containerOwner = null as any;
+	}
+
+	/// #if EDITOR
+	static __canAcceptParent() {
+		return false;
+	}
+	/// #endif
+}
