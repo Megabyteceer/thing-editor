be54df87f7a9bd304bba2c546fe035da8f48e077
Megabyteceer <vasiliy.p.kostin@gmail.com>
2023-10-05 16:24:56 +0300

    build libs resolving fix

--- thing-editor/electron-main/build.js
+++ thing-editor/electron-main/build.js
@@ -76,6 +76,7 @@ module.exports = {
 					alias: {
 						'game-root': root,
 						'games': path.resolve(__dirname, '../../games'),
+						'libs': path.resolve(__dirname, '../../libs'),
 						'thing-editor': path.resolve(__dirname, '../../thing-editor'),
 						'howler.js': 'https://cdn.jsdelivr.net/npm/howler@2.2.3/dist/howler.min.js',
 						'pixi.js': 'https://cdn.jsdelivr.net/npm/pixi.js-legacy@7.2.4/dist/pixi-legacy.min.mjs'
