52c187b659eb293dbe6242389d7e55fafad9b16e
Megabyteceer <vasiliy.p.kostin@gmail.com>
2023-11-05 14:28:30 +0300

    filter pixi warnings

---
+++ thing-editor/src/editor/warnings-filter.ts
@@ -0,0 +1,39 @@
+let filterAll = false;
+
+const filterWarnings = (args: string[]) => {
+	if(filterAll) {
+		return true;
+	}
+	filterAll = args.some(a => a.includes('PixiJS Deprecation'));
+	return filterAll;
+};
+
+const originalGroupEnd = console.groupEnd;
+console.groupEnd = () => {
+	filterAll = false;
+	originalGroupEnd.apply(console);
+
+};
+const originalWarn = console.warn;
+console.warn = (...args: string[]) => {
+	if(!filterWarnings(args)) {
+		originalWarn.apply(console, args);
+	}
+};
+const originalGroupCollapsed = console.groupCollapsed;
+console.groupCollapsed = (...args: string[]) => {
+	if(!filterWarnings(args)) {
+		originalGroupCollapsed.apply(console, args);
+	}
+};
+
+const filterDebug = (args: string[]) => {
+	return args.some(a => a.includes('[vite] connecting...') || a.includes('[vite] connected.'));
+};
+
+const originalDebug = console.debug;
+console.debug = (...args: string[]) => {
+	if(!filterDebug(args)) {
+		originalDebug.apply(console, args);
+	}
+};
