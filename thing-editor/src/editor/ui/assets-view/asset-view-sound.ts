ab9839c7176ad53b46792d53ab6a6aa6cd1af0eb
Megabyteceer <vasiliy.p.kostin@gmail.com>
2023-12-08 11:27:34 +0300

    sounds bitrates

--- thing-editor/src/editor/ui/assets-view/asset-view-sound.ts
+++ thing-editor/src/editor/ui/assets-view/asset-view-sound.ts
@@ -1,4 +1,4 @@
-import { AssetType, FileDesc } from "thing-editor/src/editor/fs";
+import fs, { AssetType, FileDesc } from "thing-editor/src/editor/fs";
 import R from "thing-editor/src/editor/preact-fabrics";
 import AssetsView, { addSharedAssetContextMenu, assetTypesIcons } from "thing-editor/src/editor/ui/assets-view/assets-view";
 import showContextMenu from "thing-editor/src/editor/ui/context-menu";
@@ -7,6 +7,10 @@ import copyTextByClick from "thing-editor/src/editor/utils/copy-text-by-click";
 import libInfo from "thing-editor/src/editor/utils/lib-info";
 import game from "thing-editor/src/engine/game";
 
+const BITRATE_PROPS = {
+	className: 'semi-transparent'
+}
+
 const assetsItemNameProps = {
 	className: 'selectable-text',
 	title: 'Ctrl+click to copy sound name',
@@ -14,19 +18,49 @@ const assetsItemNameProps = {
 };
 
 const assetItemRendererSound = (file: FileDesc) => {
+	const isDefaultBitrate = !game.editor.projectDesc.soundBitRates.hasOwnProperty(file.assetName);
 	return R.div(
 		{
 			className: (AssetsView.currentItemName === file.assetName) ? 'assets-item assets-item-current assets-item-sound' : 'assets-item assets-item-sound',
 			key: file.assetName,
 			onContextMenu: (ev: PointerEvent) => {
+				const currentBitrate = (game.editor.projectDesc.soundBitRates[file.assetName] || game.editor.projectDesc.soundDefaultBitrate);
 				showContextMenu(addSharedAssetContextMenu(file, [
 					MUTE_SOUND_MENU_ITEM,
 					{
 						name: R.fragment("Move " + file.assetName + "' sound to library..."),
-						onClick: async () => {
+						onClick: () => {
 							game.editor.moveAssetToLibrary("Where to move sound '" + file.assetName + "'?", file);
 						},
 						disabled: () => game.editor.getUserVisibleFolders().length < 2
+					},
+					{
+						name: R.span({ className: isDefaultBitrate ? 'semi-transparent' : undefined },
+							"Bitrate ",
+							currentBitrate,
+							"kbps",
+							isDefaultBitrate ? " (default)" : undefined,
+							' ▾'
+						),
+						onClick: (ev?: PointerEvent) => {
+							showContextMenu([8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160, 192, 224, 256].map((bitrate) => {
+								const isDefaultBitrate = bitrate === game.editor.projectDesc.soundDefaultBitrate;
+								return {
+									name: bitrate + 'kbps' + (isDefaultBitrate ? " (default)" : ""),
+									disabled: () => currentBitrate === bitrate,
+									onClick: () => {
+										if(isDefaultBitrate) {
+											delete game.editor.projectDesc.soundBitRates[file.assetName];
+										} else {
+											game.editor.projectDesc.soundBitRates[file.assetName] = bitrate;
+										}
+										game.editor.saveProjectDesc();
+										game.editor.ui.refresh();
+										fs.rebuildSounds(file.lib ? file.lib.assetsDir : game.editor.currentProjectAssetsDir);
+									}
+								}
+							}), ev!);
+						},
 					}
 				]), ev);
 			},
@@ -38,7 +72,12 @@ const assetItemRendererSound = (file: FileDesc) => {
 		},
 		libInfo(file),
 		assetTypesIcons.get(AssetType.SOUND),
-		R.span(assetsItemNameProps, file.assetName));
+		R.span(assetsItemNameProps, file.assetName,
+			isDefaultBitrate ? undefined : R.span(
+				BITRATE_PROPS, ' (', game.editor.projectDesc.soundBitRates[file.assetName], 'kbps)'
+			)
+		)
+	);
 };
 
 export default assetItemRendererSound;
