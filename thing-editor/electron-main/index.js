ebcc43337e7690f9a10f95b1504fa1e6c06be4fc
Megabyteceer <vasiliy.p.kostin@gmail.com>
2024-03-23 00:36:18 +0300

    do not load fonts in ci mode

--- thing-editor/electron-main/index.js
+++ thing-editor/electron-main/index.js
@@ -101,7 +101,7 @@ const createWindow = () => {
 
 	require('./server-fs.js')(mainWindow);
 
-	const EDITOR_VITE_ROOT = 'http://localhost:5173/thing-editor/';
+	const EDITOR_VITE_ROOT = 'http://0.0.0.0:5173/thing-editor/';
 	const loadEditorIndexHTML = () => {
 		mainWindow.setOpacity(1);
 		mainWindow.loadURL(EDITOR_VITE_ROOT);
@@ -110,7 +110,7 @@ const createWindow = () => {
 	if (IS_DEBUG) {
 
 		//let debuggerDetector = require('./debugger-detection');
-		mainWindow.loadURL('http://localhost:5173/thing-editor/debugger-awaiter.html').catch((er) => {
+		mainWindow.loadURL('http://0.0.0.0:5173/thing-editor/debugger-awaiter.html').catch((er) => {
 			mainWindow.setOpacity(1);
 			if (er.code === 'ERR_CONNECTION_REFUSED') {
 				console.error('Could not load ' + EDITOR_VITE_ROOT + '.\nDoes vite.js server started?');
