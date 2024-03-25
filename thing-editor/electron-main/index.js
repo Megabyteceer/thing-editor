cfce61c7a31ecfdbdafc5fe2260c693bbd08b8cf
Megabyteceer <vasiliy.p.kostin@gmail.com>
2024-03-26 01:09:12 +0300

    electron 19 test

--- thing-editor/electron-main/index.js
+++ thing-editor/electron-main/index.js
@@ -46,7 +46,7 @@ const createWindow = () => {
 		webPreferences: {
 			preload: path.join(__dirname, 'preload.js'),
 			additionalArguments: [
-				IS_CI_RUN ? '--js-flags="--max_old_space_size=2048' : '--js-flags="--max_old_space_size=8192',
+				IS_CI_RUN ? '--js-flags="--max_old_space_size=8192' : '--js-flags="--max_old_space_size=8192',
 				'--user-data-dir=' + path.join(os.tmpdir(), 'chrome-user-tmp-data')
 				//"--wait-for-debugger"
 			],
