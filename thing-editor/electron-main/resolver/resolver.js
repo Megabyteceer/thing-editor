1bb8ce81cb115fdd5ba3a9f3bfcbcbde17da6835
Megabyteceer <vasiliy.p.kostin@gmail.com>
2024-03-25 17:14:23 +0300

    CI final fix

--- thing-editor/electron-main/resolver/resolver.js
+++ thing-editor/electron-main/resolver/resolver.js
@@ -1,7 +1,33 @@
 const moduleImportFixer = /(^\s*import.+from\s*['"][^'"]+)(['"])/gm;
+
+const isCIrun = process.env.ELECTRON_ENABLE_LOGGING==='true';
+
+const queue = [];
+
+if (isCIrun) {
+	setInterval(() => {
+		if (queue.length) {
+			const o = queue.shift();
+			o.next();
+			console.log('resolved ' + o.url);
+		}
+
+	}, 50); // delay each response to prevent CI chrome crash
+}
+
 module.exports = {
 	name: 'thing-editor:resolver',
+
+	configureServer(server) {
+		if (isCIrun) {
+			server.middlewares.use((_req, _res, next) => {
+				queue.push({next, url: _req.originalUrl});
+			});
+		}
+	},
+
 	transform(src, id) {
+
 		if (id.indexOf('.ts?') > 0) {
 			let query = id.split('.ts?')[1];
 			query = '?' + query;
@@ -13,10 +39,10 @@ module.exports = {
 				return m1 + query + m2;
 			});
 		}
+
 		return {
 			code: src,
 			map: null
 		};
 	}
 };
-
