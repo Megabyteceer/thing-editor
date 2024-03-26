b378f5c47d5243bb6cba520f7c2b18a7b324d923
Megabyteceer <vasiliy.p.kostin@gmail.com>
2024-03-26 17:56:38 +0300

    resolver fix

--- thing-editor/electron-main/resolver/resolver.js
+++ thing-editor/electron-main/resolver/resolver.js
@@ -2,6 +2,11 @@ const moduleImportFixer = /(^\s*import.+from\s*['"][^'"]+)(['"])/gm;
 
 const IS_CI_RUN = process.env.IS_CI_RUN === 'true';
 
+import path from 'path';
+
+const gamesPath = path.join(__dirname, '../../../games').replaceAll('\\', '/') + '/';
+const libsPath = path.join(__dirname, '../../../libs').replaceAll('\\', '/') + '/';
+
 const queue = [];
 
 if (IS_CI_RUN) {
@@ -25,15 +30,24 @@ module.exports = {
 
 	transform(src, id) {
 
-		if (id.indexOf('.ts?') > 0) {
+		if (id.includes('.ts?')) {
+
 			let query = id.split('.ts?')[1];
 			query = '?' + query;
 
 			src = src.replace(moduleImportFixer, (_substr, m1, m2) => {
-				if (m1.indexOf('from \'games/') < 0 && m1.indexOf('from \'libs/') < 0) {
-					return m1 + m2;
+				if (!_substr.includes('.css?inline"')) {
+					if (m1.includes('".')) { // relative imports treats games or libs if imported from games or libs
+						if (id.startsWith(gamesPath) || id.startsWith(libsPath)) {
+						//	return m1 + query + m2;
+						}
+					} else {
+						if (m1.includes('from \"games/') || m1.includes('from \"libs/')) {
+						//	return m1 + query + m2;
+						}
+					}
 				}
-				return m1 + query + m2;
+				return _substr;
 			});
 		}
 
