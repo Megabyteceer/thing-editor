c48d77e781a624b67ba36f659e5fbc762a75b850
Megabyteceer <vasiliy.p.kostin@gmail.com>
2024-03-27 01:00:01 +0300

    do not patch typings on CI

--- thing-editor/electron-main/pixi-typings-patch.js
+++ thing-editor/electron-main/pixi-typings-patch.js
@@ -1,6 +1,6 @@
 // add vscode intellisense for injected methods
 
-const IS_CI_RUN = !!process.env.CI_COMMIT_REF_NAME;
+const IS_CI_RUN = process.env.IS_CI_RUN === 'true';
 
 let patches = [];
 const PATCH_BEGIN = ` // thing-editor patch begin
