bad95758811b071cf6556aa6a555ee4ac7225480
Megabyteceer <vasiliy.p.kostin@gmail.com>
2024-03-30 18:45:16 +0300

    ESlint decorators issue temporary fix https://github.com/eslint/eslint/issues/15299#issuecomment-968099681

--- thing-editor/src/engine/lib/assets/src/basic/fill.c.ts
+++ thing-editor/src/engine/lib/assets/src/basic/fill.c.ts
@@ -152,10 +152,10 @@ export default class Fill extends Mesh {
 	}
 
 	@editable({ step: 0.0001 })
-		xShiftSpeed = 0;
+	xShiftSpeed = 0;
 
 	@editable({ step: 0.0001 })
-		yShiftSpeed = 0;
+	yShiftSpeed = 0;
 
 	/// #if EDITOR
 	@editable(TEXTURE_WRAP_MODE_DESC)
@@ -217,7 +217,7 @@ export default class Fill extends Mesh {
 	}
 
 	@editable({ step: 0.0001, visible: o => o.xWaveAmp !== 0 })
-		xWaveSpeed = 0;
+	xWaveSpeed = 0;
 
 	_yWaveAmp = 0;
 	@editable({ step: 0.001 })
@@ -259,7 +259,7 @@ export default class Fill extends Mesh {
 	}
 
 	@editable({ step: 0.0001, visible: o => o.yWaveAmp !== 0 })
-		yWaveSpeed = 0;
+	yWaveSpeed = 0;
 
 	transparencyUpdated = false;
 	_transparentTop = false;
