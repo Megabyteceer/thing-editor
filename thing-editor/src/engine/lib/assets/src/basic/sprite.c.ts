7d0d8f83f8181b344d60affbe3eb9caff2284f44
Megabyteceer <vasiliy.p.kostin@gmail.com>
2024-03-30 19:04:42 +0300

    extensions reccomendations

--- thing-editor/src/engine/lib/assets/src/basic/sprite.c.ts
+++ thing-editor/src/engine/lib/assets/src/basic/sprite.c.ts
@@ -22,9 +22,9 @@ const imageJSPropertyDescriptor = {
 				if (this.anchor && ((((this.texture.height & 1) !== 0) && this.anchor.x === 0.5) || (((this.texture.width & 1) !== 0) && this.anchor.y === 0.5))) {
 					game.editor.ui.status.warn('Texture "' + v + '" has non even sized bounds ('
 						+ this.texture.width + 'x' + this.texture.height + '). It is can cause unwanted blurring for objects with centralized pivot point.', 32028,
-						() => {
-							game.editor.editSource(fs.getFileByAssetName(v, AssetType.IMAGE).fileName);
-						});
+					() => {
+						game.editor.editSource(fs.getFileByAssetName(v, AssetType.IMAGE).fileName);
+					});
 				}
 			}
 			/// #endif
