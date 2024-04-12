05765adb4494cb10979d83cd4e7ed8f36ca102fd
Megabyteceer <vasiliy.p.kostin@gmail.com>
2024-04-12 14:32:24 +0300

    unhide generated files to make branches switch easier

--- thing-editor/electron-main/assume-unchanged.js
+++ thing-editor/electron-main/assume-unchanged.js
@@ -2,7 +2,7 @@ const {exec} = require('child_process');
 
 module.exports = async function (log, enable) {
 
-	const command = 'git update-index --' + (enable ? '' : 'no-') + 'assume-unchanged thing-editor.code-workspace tsconfig.json';
+	const command = 'git update-index --' + (enable ? '' : 'no-') + 'assume-unchanged thing-editor.code-workspace tsconfig.json thing-editor/src/editor/current-scene-typings.d.ts thing-editor/src/editor/prefabs-typing.ts';
 	log(command);
 
 	exec(command, {
