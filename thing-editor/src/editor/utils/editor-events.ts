7ad48ad2c7c71031e4f5a6302427ec6c675c6a43
Megabyteceer <vasiliy.p.kostin@gmail.com>
2024-07-04 23:56:08 +0300

    editorEvents warning about many times handler will be added in game dynamic code

--- thing-editor/src/editor/utils/editor-events.ts
+++ thing-editor/src/editor/utils/editor-events.ts
@@ -14,6 +14,28 @@ type EditorEvents = {
 	soundPlay: (soundId: string, volume: number) => void;
 };
 
+/** Warning! Events handlers can be added more that once because of dynamic classes reloading.
+ * Please ensure you adding handlers with checking if you not added it already.
+ * As example you can store a flag some where in global object:
+ *
+`````````
+import { editorEvents } from 'thing-editor/src/editor/utils/editor-events';
+import EDITOR_FLAGS from 'thing-editor/src/editor/utils/flags';
+/// #if EDITOR
+// this code will be called after each classes reloading so you will add the same handler many times
+
+if (!(EDITOR_FLAGS as any).__myClassesReloadingHandlerAdded) {
+	editorEvents.on('willClassesReload', () => {
+		console.log('fire once per classes reloading');
+	});
+	(EDITOR_FLAGS as any).__myClassesReloadingHandlerAdded = true;
+}
+editorEvents.on('willClassesReload', () => {
+	console.log('fire as many times per each classes reloading, as many time you have reloaded them.');
+});
+/// #endif
+`````````
+ * */
 const editorEvents = new EventEmitter() as TypedEventEmitter<EditorEvents>;
 
 export { editorEvents };
