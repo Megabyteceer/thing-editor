82a632b7f6cfb0403d79a24f53e04294beff5f65
Megabyteceer <vasiliy.p.kostin@gmail.com>
2024-07-08 13:37:07 +0300

    deepFreeze debug only usage tip

--- thing-editor/src/engine/utils/deep-freeze.ts
+++ thing-editor/src/engine/utils/deep-freeze.ts
@@ -2,7 +2,7 @@ const deepFreeze = (object: any) => {
 	/// #if DEBUG
 	/*
 	/// #endif
-	throw Error('deepFreeze is for debug build only.');
+	throw Error('"deepFreeze" is for debug build only. Please wrap "deepFreeze" usage in "///#if DEBUG" statement');
 	//*/
 
 	const propNames = Reflect.ownKeys(object);
