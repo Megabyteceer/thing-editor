ede6941705f03924f2b8b8f3eec6596d730e9e67
Megabyteceer <vasiliy.p.kostin@gmail.com>
2024-10-16 09:10:30 +0300

    OrientationParentResizer fix

--- thing-editor/src/engine/lib/assets/src/common/ui/orientation-parent-resizer.c.ts
+++ thing-editor/src/engine/lib/assets/src/common/ui/orientation-parent-resizer.c.ts
@@ -1,4 +1,3 @@
-import type UIContainer from 'libs/thing-games-utils/common/assets/src/ui/u-i-container.c';
 import OrientationTrigger from 'thing-editor/src/engine/lib/assets/src/mobile/orientation-trigger.c';
 import type Shape from '../../extended/shape.c';
 
@@ -6,9 +5,9 @@ export default class OrientationParentResizer extends OrientationTrigger {
 	applyOrientation(): void {
 		super.applyOrientation();
 		if (this.parent) {
-			if ((this.parent as UIContainer).W) {
-				(this.parent as UIContainer).W = this.x;
-				(this.parent as UIContainer).H = this.y;
+			if ((typeof (this.parent as any).W === 'number')) {
+				(this.parent as any).W = this.x;
+				(this.parent as any).H = this.y;
 			} else {
 				(this.parent as Shape).width = this.x;
 				(this.parent as Shape).height = this.y;
