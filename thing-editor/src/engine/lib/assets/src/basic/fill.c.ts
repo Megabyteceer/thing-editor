797fcb672380b397d282230f74ac8c94c288ecd8
Megabyteceer <vasiliy.p.kostin@gmail.com>
2024-11-17 22:12:48 +0300

    wrap mode for EMPTY texture fix

--- thing-editor/src/engine/lib/assets/src/basic/fill.c.ts
+++ thing-editor/src/engine/lib/assets/src/basic/fill.c.ts
@@ -159,7 +159,7 @@ export default class Fill extends Mesh {
 	/// #if EDITOR
 	@editable(TEXTURE_WRAP_MODE_DESC)
 	set TEXTURE_WRAP_MODE(v: number) {
-		if (this.texture) {
+		if (this.texture && this.image) {
 			let bits = 0;
 			if (v === WRAP_MODES.REPEAT) {
 				bits = 8;
