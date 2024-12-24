11586410ec7eb1e81eff69f306d31ba679d902d4
Megabyteceer <vasiliy.p.kostin@gmail.com>
2024-12-24 09:46:54 +0300

    remove configs from repo

--- thing-editor/demo/install.js
+++ thing-editor/demo/install.js
@@ -5,11 +5,18 @@ import * as patcher from '../electron-main/pixi-typings-patch.js';
 
 patcher.default();
 
-if (!fs.existsSync('./games')) {
+if(!fs.existsSync('./games')) {
 	fs.mkdirSync('./games');
 }
-if (!fs.existsSync('./libs')) {
+if(!fs.existsSync('./libs')) {
 	fs.mkdirSync('./libs');
+}
+
+if(!fs.existsSync('./thing-editor.code-workspace')) {
+	fs.copyFileSync('./thing-editor.code-workspace.template', './thing-editor.code-workspace');
+}
+if(!fs.existsSync('./tsconfig.json')) {
+	fs.copyFileSync('./tsconfig.json.template', './tsconfig.json');
 }
 
 fs.cpSync('./thing-editor/demo/example-project', './games/example-project', {recursive: true, force: false});
