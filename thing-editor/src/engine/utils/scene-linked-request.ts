3eef5ec38644a18294e9e9a03c30b3771cc91bb9
Vasilii Kostin <vasili.kostin@bgaming.com>
2025-05-07 16:46:53 +0300

    SceneLinkedRequest queued

--- thing-editor/src/engine/utils/scene-linked-request.ts
+++ thing-editor/src/engine/utils/scene-linked-request.ts
@@ -1,10 +1,14 @@
 import type { Container } from 'pixi.js';
 import assert from 'thing-editor/src/engine/debug/assert';
 import SceneLinkedPromise from '../lib/assets/___system/scene-linked-promise.c';
+import waitForCondition from '../lib/assets/src/utils/wait-for-condition';
 
 const hooks:SceneLinkedRequestHook[] = [];
 
 export type SceneLinkedRequestHook = (url: string, resultFormat: 'json' | 'text' | 'blob' | 'arrayBuffer', options?: RequestInit) => (Promise<void> | undefined);
+
+
+let queuedInProgress = false;
 
 export default class SceneLinkedRequest {
 
@@ -12,7 +16,7 @@ export default class SceneLinkedRequest {
 		hooks.push(hook);
 	}
 
-	static fetch(owner: Container, url: string, resultFormat: ('json' | 'text' | 'blob' | 'arrayBuffer') = 'json', options?: RequestInit, attempts = 1) {
+	static fetch(owner: Container, url: string, resultFormat: ('json' | 'text' | 'blob' | 'arrayBuffer') = 'json', options?: RequestInit, attempts = 1, queued = false) {
 		let destroyed = false;
 		assert(owner, 'Request\'s owner display object should be provided.', 10062);
 		assert(url, 'Request\'s URL should be provided.', 10063);
@@ -20,6 +24,19 @@ export default class SceneLinkedRequest {
 			let delay = 1000;
 			if (hooks.length) {
 				await Promise.all(hooks.map(hook => hook(url, resultFormat, options)));
+			}
+
+			if (queued) {
+				if (queuedInProgress) {
+					await waitForCondition(() => {
+						if (!queuedInProgress) {
+							queuedInProgress = true;
+							return true;
+						}
+					});
+				} else {
+					queuedInProgress = true;
+				}
 			}
 			while (attempts > 0 && !destroyed) {
 				await fetch(url, options)
@@ -37,6 +54,9 @@ export default class SceneLinkedRequest {
 					});
 				await new Promise((resolve) => { window.setTimeout(resolve, delay); });
 				delay += 1000;
+			}
+			if (queued) {
+				queuedInProgress = false;
 			}
 		}, owner);
 		ret.name = 'Request: ' + url;
