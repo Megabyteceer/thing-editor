1ec76d3345005c190157630dbed545c70d300add
Vasilii Kostin <vasili.kostin@bgaming.com>
2025-08-08 11:29:13 +0300

    data path choosing header readability fix

--- thing-editor/src/editor/editor-env.d.ts
+++ thing-editor/src/editor/editor-env.d.ts
@@ -233,7 +233,7 @@ interface SelectableProperty extends AnyType {
 	___EDITOR_isHiddenForDataChooser?: true;
 	___EDITOR_isGoodForChooser?: true;
 	___EDITOR_isGoodForCallbackChooser?: true;
-	___EDITOR_ChooserOrder?: true;
+	___EDITOR_ChooserOrder?: number;
 	___EDITOR_actionIcon?: import('preact').ComponentChild;
 	___EDITOR_callbackParameterChooserFunction?: (owner: any) => Promise<any[] | any>;
 }
